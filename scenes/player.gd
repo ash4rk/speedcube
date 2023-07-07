dacd1217a4e318fbcd4e89177eb29e5be0e28bb9
ash4rk <cool.shark.98@mail.ru>
2023-07-07 15:07:01 +0300

    FEAT: Implement cube speed change

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,21 +2,25 @@ extends CharacterBody3D
 
 class_name Player
 
-const SPEED = 5.0
+@export var SPEED_VALUES = {1: 3.0, 
+							2: 10.0, 
+							3: 15.0,
+							4: 25.0}
 
 @onready var controller = $Controller
 
 func _physics_process(delta):
-		_process_movement(delta)
+	_process_movement(delta)
 
 func _process_movement(delta):
 	var input_dir = controller.input_dir
+	var speed = SPEED_VALUES[controller.input_speed]
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = -direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = -direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0,speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 
 	move_and_slide()
